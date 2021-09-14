68c26e4abba157382756fe0b5e847db0c0bfe84e
Hector Alcazar <hiad99@gmail.com>
2021-09-14 10:42:37 -0500

    First commit

---
+++ stories/badge.stories.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/html';
+import buildAndRegisterCustomElement from './utils/build-custom-element';
+import Badge from 'c/badge';
+
+buildAndRegisterCustomElement('c-badge', Badge);
+
+storiesOf('Badge', module)
+    .add('default', () => `
+        <div class="slds-p-around_medium">
+            <c-badge label="Default"></c-badge>
+        </div>    
+    `)
+    .add('inverse', () => `
+        <div class="slds-p-around_medium">
+            <c-badge variant="inverse" label="Inverse"></c-badge>
+        </div>    
+    `)
+    .add('lightest', () => `
+        <div class="slds-p-around_medium">
+            <c-badge variant="lightest" label="Lightest"></c-badge>
+        </div>    
+    `);    
