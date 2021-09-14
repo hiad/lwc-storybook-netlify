f44ff28a37a8f542e26d595c6aaebdd0884d1dc3
Hector Alcazar <hiad99@gmail.com>
2021-09-14 11:40:04 -0500

    Knbos

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,35 +1,27 @@
 import { storiesOf } from '@storybook/html';
 import buildAndRegisterCustomElement from './utils/build-custom-element';
 import Button from 'c/button';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 buildAndRegisterCustomElement('c-button', Button);
 
+const buttonFxn = () => {
+    const test = text('label', 'Button Text');
+    const variant = select('variant', ['brand',
+        'destructive',
+        'brand-outline',
+        'destructive-text',
+        'neutral',
+        'success'])
+    return `
+    <div class="slds-p-around_medium">
+    <c-button variant='${variant}' label='${test}'></c-button>
+    </div>`
+};
+
 storiesOf('Button', module)
-    .add('base', () => `
-        <div class="slds-p-around_medium">
-            <c-button label="Base Button"></c-button>
-        </div>    
-    `)
-    .add('neutral', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="neutral" label="Neutral Button"></c-button>
-        </div>    
-    `)
-    .add('brand', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="brand" label="Brand Button"></c-button>
-        </div>    
-    `)
-    .add('destructive', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="destructive" label="Destructive Button"></c-button>
-        </div>    
-    `)
-    .add('success', () => `
-        <div class="slds-p-around_medium">
-            <c-button variant="success" label="Success Button"></c-button>
-        </div>    
-    `);
+    .addDecorator(withKnobs)
+    .add('base', buttonFxn);
 
 
-   
+
